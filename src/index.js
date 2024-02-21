f179445e013a7fc6e470ba4ea8d060078a5b7775
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2024-02-21 12:48:08 +0100

    Init redux store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store'
 import App from './App';
 import 'font-awesome/css/font-awesome.min.css';
 import './styles/normalize.scss';
@@ -8,6 +10,8 @@ import './styles/global.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
 
