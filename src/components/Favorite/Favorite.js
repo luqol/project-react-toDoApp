5470f171a67bc29ffdd9a6fac1326a230cce91de
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2024-02-29 20:04:53 +0100

    Add noCards info to favorite page

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -3,18 +3,28 @@ import PageTitle from '../PageTitle/PageTitle';
 import styles from './Favorite.module.scss';
 import { getFavoriteCards } from '../../redux/cardsRedux';
 import Card from '../Card/Card';
+import { toBeEmpty } from '@testing-library/jest-dom/dist/matchers';
 
 const Favorite = () => {
   const favoriteCards = useSelector( getFavoriteCards );
-
-  return(
-    <div className={styles.favorite}>
-      <PageTitle>Favorite</PageTitle>
-      <ul className={styles.favoriteCards}>
-        {favoriteCards.map(card => <Card key={card.id} id={card.id} title={card.title}  isFavorite={card.isFavorite}  />)}
-      </ul>
-  </div>
-  );
+  if (Object.keys(favoriteCards).length !== 0){
+    return(
+      <div className={styles.favorite}>
+        <PageTitle>Favorite</PageTitle>
+        <ul className={styles.favoriteCards}>
+          {favoriteCards.map(card => <Card key={card.id} id={card.id} title={card.title}  isFavorite={card.isFavorite}  />)}
+        </ul>
+    </div>
+    );
+  } else {
+    return(
+      <div className={styles.favorite}>
+        <PageTitle>Favorite</PageTitle>
+        <p className={styles.noCards}>No Cards...</p>
+    </div>
+    );
+  }
+  
 };
 
 export default Favorite;
